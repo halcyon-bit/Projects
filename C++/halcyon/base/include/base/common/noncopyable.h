15db1cb8b232165a00b8ccb93f09505ca95baeda
gjq <329707880@qq.com>
2022-07-24 16:35:51 +0800

    add c++ base64

---
+++ C++/halcyon/base/include/base/common/noncopyable.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include <base/common/base_define.h>
+
+BASE_BEGIN_NAMESPACE
+
+/**
+ * @brief   对象语义的基类
+ */
+class noncopyable
+{
+public:
+    noncopyable(const noncopyable&) = delete;
+    void operator=(const noncopyable&) = delete;
+
+protected:
+    noncopyable() = default;
+    ~noncopyable() = default;
+};
+
+BASE_END_NAMESPACE
