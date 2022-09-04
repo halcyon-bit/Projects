371ba6d7eb8e51a1efccdcfe37636475d83186d7
gjq <329707880@qq.com>
2022-09-05 00:01:27 +0800

    update version

---
+++ C++/halcyon/base/src/version.cpp
@@ -0,0 +1,16 @@
+﻿#include "base/common/base_define.h"
+
+BASE_BEGIN_NAMESPACE
+
+#define BASE_VERSION_MAJOR  "0"
+#define BASE_VERSION_MINOR  "0"
+#define BASE_VERSION_PATCH  "1"
+
+#define BASE_VERSION    "v" BASE_VERSION_MAJOR "." BASE_VERSION_MINOR "." BASE_VERSION_PATCH
+
+const char* version()
+{
+    return BASE_VERSION;
+}
+
+BASE_END_NAMESPACE
